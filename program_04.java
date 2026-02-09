1e38e7c5762908f096fab962135904bd0e865a9f
VinayPapnoi <papnoi.vinay@gmail.com>
2026-02-09 14:52:03 +0530

    added 4th program

---
+++ program_04.java
@@ -0,0 +1,16 @@
+//WAP in java to swap two numbers
+
+import java.util.*;
+public class program_04 {;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.print("The number is ");
+        if(a%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
---
+++ program_05.java
@@ -0,0 +1,5 @@
+//WAP in java to check even number
+
+public class program_05 {
+    
+}
